df10b2379453f38938cf0e5df8307e4ce589e95e
Jamison Burton <jamison@burtontalk.com>
2022-01-13 08:52:01 -0700

    First submission

--- Variables/Variables.vb
+++ Variables/Variables.vb
@@ -1,6 +1,31 @@
-﻿Module Variables
+﻿Option Explicit On
+Module Variables
 
     Sub Main()
+        'declaring some variables
+        Dim firstNumber As Integer
+        Dim secondNumber As Decimal 'Use a D or @ to assign the decimal type
+        Dim result As Decimal
+        Dim testResult As Boolean
+        Dim userInput As String
+        Const TAXRATE As Decimal = 0.06D
+
+        firstNumber = 5
+        secondNumber = 38.27D
+        result = firstNumber + secondNumber
+
+        ''display result to user
+        'Console.WriteLine(result)
+        'userInput = Console.ReadLine()
+
+        ''shows what the user typed
+        'Console.WriteLine(userInput)
+        'Console.ReadLine()
+
+        testResult = (5 > 4)
+
+        Console.WriteLine(testResult)
+        Console.ReadLine()
 
     End Sub
 
